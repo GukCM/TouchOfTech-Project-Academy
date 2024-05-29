12a7e5c69b92bf78a9a4b491b75bc023a11fb425
JuanRMata <rodry.mata@gmail.com>
2024-05-29 16:30:39 -0600

    casi finalizado ruta payment y ajustes en colores de background en todo el documento

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -6,30 +6,30 @@ const Layout = () => {
   const location = useLocation();
   return (
     <div className="min-h-screen flex flex-col sticky">
-      <nav className=" bg-white text-black flex items-center py-4 px-60 sticky top-0 left-0 w-full z-10">
+      <nav className=" bg-gray-100 text-black flex items-center py-4 px-60 sticky top-0 left-0 w-full z-10">
         <div className="flex items-center">
           <Link to="/" className= {`hover:text-green-200 transition-all ${location.pathname === '/' ? 'relative text-green-500' : ''}`}>
             <img src={logo} alt="Logo" className="h-10 mr-4" />
           </Link>
         </div>
           <div className="flex space-x-10 w-full justify-center">
-            <Link to="/" className= {`hover:text-green-200 transition-all ${location.pathname === '/' ? 'relative text-green-500' : ''}`}>
+            <Link to="/" className= {`hover:text-green-200 transition-all font-semibold ${location.pathname === '/' ? 'relative text-green-500' : ''}`}>
               Home
               {location.pathname === "/" && (
                 <span className="absolute h-0.5 bg-green-500 bottom-0 left-0 w-full"></span>
               )} 
             </Link>
             
-            <Link to="/register" className="hover:text-green-200 transition-all">Sign Up</Link>
-            <Link to="/login" className="hover:text-green-200 transition-all">Login</Link>
-            <Link to="/cart" className={`hover:text-green-200 transition-all ${location.pathname === '/cart' ? 'relative text-green-500' : ''}`}>
+            <Link to="/register" className="hover:text-green-200 transition-all font-semibold">Sign Up</Link>
+            <Link to="/login" className="hover:text-green-200 transition-all font-semibold">Login</Link>
+            <Link to="/cart" className={`hover:text-green-200 transition-all font-semibold ${location.pathname === '/cart' ? 'relative text-green-500' : ''}`}>
               Cart
               {location.pathname === "/cart" && (
                 <span className="absolute h-0.5 bg-green-500 bottom-0 left-0 w-full"></span>
               )} 
             </Link>
 
-            <Link to= "/products" className={`hover:text-green-200 transition-all ${location.pathname === '/products' ? 'relative text-green-500' : ''}`}>
+            <Link to= "/products" className={`hover:text-green-200 transition-all font-semibold ${location.pathname === '/products' ? 'relative text-green-500' : ''}`}>
               Products
               {location.pathname === "/products" && (
                 <span className="absolute h-0.5 bg-green-500 bottom-0 left-0 w-full"></span>
